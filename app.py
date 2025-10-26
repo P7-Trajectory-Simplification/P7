be1daeb767d04d92cac6069b5704ce73213a765c
MarkusHundborg <mhundb22@student.aau.dk>
2025-10-26 10:26:13 +0100

    Add compression ratio calculation to error metrics and update analytics table

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ from classes.vessel import Vessel
 from datetime import datetime
 from data.vessel_cache import get_data_from_cache
 from typing import Callable
+from error_metrics.comp_ratio import comp_ratio_results
 from error_metrics.sed import sed_results
 from error_metrics.ped import ped_results
 
@@ -90,7 +91,8 @@ def get_error_metrics(raw_routes: list[Route], simplified_routes: list[Route]) -
     error_metrics = []
     ped_avg, ped_max = ped_results(raw_routes, simplified_routes)
     sed_avg, sed_max = sed_results(raw_routes, simplified_routes)
-    error_metrics = [ped_avg, ped_max, sed_avg, sed_max]
+    comp_ratio = comp_ratio_results(raw_routes, simplified_routes)
+    error_metrics = [ped_avg, ped_max, sed_avg, sed_max, comp_ratio]
     return error_metrics
 
 
--- static/JS/analytics.js
+++ static/JS/analytics.js
@@ -12,11 +12,11 @@ function get_metrics(algorithm, all_error_metrics) {
 }
 
 function create_table(all_error_metrics) {
-    error_metrics = ['SED avg.', 'SED max', 'PED avg.', 'PED max']
+    error_metrics = ['SED avg.', 'SED max', 'PED avg.', 'PED max', 'Compression Ratio'];
     table = document.createElement('table');
     const tr = table.insertRow();
     const td = tr.insertCell();
-    for(let i = 0; i <= 3; i++) {
+    for(let i = 0; i <= 4; i++) {
         const th = document.createElement('th');
         tr.appendChild(th);
         th.textContent = error_metrics[i];
