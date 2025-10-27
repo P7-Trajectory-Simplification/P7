01f23d41a0427631b3a6b52d950af69e17e76a83
Julius <julius.jensen02@gmail.com>
2025-10-27 08:07:26 +0100

    Init

--- static/JS/api.js
+++ static/JS/api.js
@@ -6,22 +6,19 @@ function algorithm_request(callback = null) {
     if (algorithms.length < 1) return;
     
     request("algorithm", {algorithms: algorithms.join(","), start_date: start_date, end_date: end_date}, (data) => {
-        let squish_data = data.SQUISH;
-        let squish_e_data = data.SQUISH_E;
-        let dp_data = data.DP;
-        let dr_data = data.DR;
-        let squish_reckoning_data = data.SQUISH_RECKONING;
-        let raw_data = data.raw;
-        let all_error_metrics = {DP: data.DP_error_metrics, DR: data.DR_error_metrics, SQUISH: data.SQUISH_error_metrics};
-        create_table(all_error_metrics);
+        create_table({
+            DP: data.DP_error_metrics,
+            DR: data.DR_error_metrics,
+            SQUISH: data.SQUISH_error_metrics
+        });
 
         clear_map();
-        plot_to_map(squish_data, "green");
-        plot_to_map(squish_e_data, "cyan")
-        plot_to_map(squish_reckoning_data, 'magenta')
-        plot_to_map(raw_data, "blue");
-        plot_to_map(dp_data, "red");
-        plot_to_map(dr_data, "yellow");
+        plot_to_map(data.SQUISH, "green");
+        plot_to_map(data.SQUISH_E, "cyan")
+        plot_to_map(data.SQUISH_RECKONING, 'magenta')
+        plot_to_map(data.raw, "blue");
+        plot_to_map(data.DP, "red");
+        plot_to_map(data.DR, "yellow");
 
         if (callback && callback instanceof Function) callback();
     });
@@ -33,7 +30,13 @@ function request(path, params, callback) {
         parameters += key + "=" + params[key] + "&";
     }
     parameters = parameters.slice(0, -1);
-    fetch("/"+path + parameters)
+    fetch("/"+path + parameters, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body
+    })
         .then(response => response.json())
         .then(data => callback(data))
         .catch((error) => {
