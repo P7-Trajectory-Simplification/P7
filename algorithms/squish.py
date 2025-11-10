0c6e1916bcf9edaaea59918a7ee9b90102059641
Julius <jj@hefa.dk>
2025-11-10 15:26:15 +0100

    Merged main into branch to implement for squish

--- algorithms/squish.py
+++ algorithms/squish.py
@@ -1,65 +1,78 @@
-from algorithms.great_circle_math import (
-    great_circle_distance
-)
+from algorithms.great_circle_math import great_circle_distance
 from classes.route import Route
+from classes.simplifier import Simplifier
 from classes.vessel_log import VesselLog
 from classes.priority_queue import PriorityQueue
 import numpy as np
 
-
-def update_sed(index: int, trajectory: list[VesselLog], heap: PriorityQueue, succ: dict, pred: dict):
-    if index in pred and index in succ:
-        a = trajectory[pred[index]]
-        b = trajectory[succ[index]]
-        target = trajectory[index]
-        
-        if target.ts - a.ts < b.ts - target.ts:
-            # Closer to a
-            sed = np.abs(great_circle_distance(a.get_coords(), target.get_coords()))
-        else:
-            # Closer to b
-            sed = np.abs(great_circle_distance(b.get_coords(), target.get_coords()))
-
-        heap.insert(index, target, sed)
-        
-
-
-
-def squish(trajectory: list[VesselLog], buff: list[VesselLog], buff_size: int = 100):
-    heap = PriorityQueue()
-    succ = {}
-    pred = {}
-
-    for i in range(len(trajectory)):
-        heap.insert(i, trajectory[i], float('inf'))
-
-        if i > 0: #After the first point
-            succ[i - 1] = i
-            pred[i] = i - 1
-
-        if heap.size() >= 3:
-            update_sed(i-1, trajectory, heap, succ, pred)
-
-
-        if heap.size() == buff_size + 1:
-            index, _, _ = heap.remove_min()
-            
-            succ[pred[index]] = succ[index]
-            pred[succ[index]] = pred[index]
-            del pred[index]; del succ[index]
-
-            if index in pred and index in succ:
-                update_sed(pred[index], trajectory, heap, succ, pred)
-                update_sed(succ[index], trajectory, heap, succ, pred)
-    
-    #The first point is the one with 0 predecessors
-    start = next((pid for pid in pred if pred[pid] is None), 0)
-    curr = start
-    while curr is not None: #Add each point in order
-        buff.append(trajectory[curr])
-        curr = succ.get(curr)
-
+singleton = None
 
 def run_squish(route: Route, params: dict) -> Route:
-    squish(route.trajectory, route.squish_buff, params["buff_size"])
-    return Route(route.squish_buff)
+    global singleton
+    if singleton is None:
+        singleton = Squish(params["buff_size"])
+    squish = singleton
+
+    for vessel_log in route.trajectory:
+        squish.trajectory.append(vessel_log)
+        squish.simplify()
+    return Route(squish.trajectory)
+
+class Squish(Simplifier):
+    def __init__(self, buffer_size: int = 100):
+        super().__init__()
+        self.buffer_size = buffer_size
+        self.buffer: list[VesselLog] = []
+        self.heap = PriorityQueue()
+        self.successor = {}
+        self.predecessor = {}
+
+    def simplify(self):
+        self.trajectory = self.squish(self.trajectory)
+
+    def update_sed(self, index: int):
+        if index in self.predecessor and index in self.successor:
+            a = self.trajectory[self.predecessor[index]]
+            b = self.trajectory[self.successor[index]]
+            target = self.trajectory[index]
+
+            if target.ts - a.ts < b.ts - target.ts:
+                # Closer to a
+                sed = np.abs(great_circle_distance(a.get_coords(), target.get_coords()))
+            else:
+                # Closer to b
+                sed = np.abs(great_circle_distance(b.get_coords(), target.get_coords()))
+
+            self.heap.insert(index, target, sed)
+
+    def squish(self, trajectory: list[VesselLog]):
+        for i in range(len(trajectory)):
+            self.heap.insert(i, trajectory[i], float('inf'))
+
+            if i > 0: #After the first point
+                self.successor[i - 1] = i
+                self.predecessor[i] = i - 1
+
+            if self.heap.size() >= 3:
+                self.update_sed(i-1)
+
+
+            if self.heap.size() == self.buffer_size + 1:
+                index, _, _ = self.heap.remove_min()
+
+                self.successor[self.predecessor[index]] = self.successor[index]
+                self.predecessor[self.successor[index]] = self.predecessor[index]
+                del self.predecessor[index]; del self.successor[index]
+
+                if index in self.predecessor and index in self.successor:
+                    self.update_sed(self.predecessor[index])
+                    self.update_sed(self.successor[index])
+
+        #The first point is the one with 0 predecessors
+        start = next((pid for pid in self.predecessor if self.predecessor[pid] is None), 0)
+        curr = start
+        while curr is not None: #Add each point in order
+            self.buffer.append(trajectory[curr])
+            curr = self.successor.get(curr)
+
+        return self.buffer
