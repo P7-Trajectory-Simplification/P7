c4bda2f7154a9419f5c80f3577af26f718a94d1b
Markus Holmager Hundborg <113089120+MarkusHundborg@users.noreply.github.com>
2025-11-19 10:40:36 +0100

    Update comments in VesselLog constructor
    Clarified comments for latitude and longitude attributes.

--- classes/vessel_log.py
+++ classes/vessel_log.py
@@ -4,9 +4,9 @@ import numpy as np
 
 class VesselLog:
     def __init__(self, lat: float, lon: float, ts: datetime, imo: int, id: int):
-        self.lat = lat # Latitude of the log
-        self.lon = lon # Latitude of the log
-        self.ts = ts # Timestamp of the log
+        self.lat = lat # Latitude of the log in degrees
+        self.lon = lon # Longitude of the log in degrees
+        self.ts = ts # Timestamp of the log in datetime
         self.imo = imo # IMO number of the vessel
         self.id = id # Id of the log in the database (auto incremented)
 
