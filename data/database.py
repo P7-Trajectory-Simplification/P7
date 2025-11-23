9cdf12a8df09324181a287b64d4b89b36ddfaeb1
emils <esla22@student.aau.dk>
2025-11-23 17:24:37 +0100

    added support for multiple vessels' trajectories being simplified at once. Look into performance

--- data/database.py
+++ data/database.py
@@ -48,17 +48,30 @@ def get_all_vessels() -> list[Vessel]:
     return hydrate_vessels(result.fetchall())
 
 
-def get_vessel_logs(imo: int, start_ts: datetime, end_ts: datetime) -> list[VesselLog]:
+def get_vessel_logs(
+    imo: int | list[int], start_ts: datetime, end_ts: datetime
+) -> list[VesselLog]:
     start_time = start_ts.strftime('%Y-%m-%d %H:%M:%S')
     end_time = end_ts.strftime('%Y-%m-%d %H:%M:%S')
     conn = open_connection()
-    statement = text(
-        'SELECT imo, lat, lon, ts, id FROM vessel_logs WHERE imo = :imo AND ts >= :start_ts AND ts <= :end_ts ORDER BY ts;'
-    )
-    result = conn.execute(
-        statement, {'imo': imo, 'start_ts': start_time, 'end_ts': end_time}
-    )
-    return hydrate_vessel_logs(result.fetchall())
+    if isinstance(imo, int):
+        # SECTION legacy code to keep compatibility with the cache
+        statement = text(
+            'SELECT imo, lat, lon, ts, id FROM vessel_logs WHERE imo = :imo AND ts >= :start_ts AND ts <= :end_ts ORDER BY ts;'
+        )
+        result = conn.execute(
+            statement, {'imo': imo, 'start_ts': start_time, 'end_ts': end_time}
+        )
+        return hydrate_vessel_logs(result.fetchall())
+    else:
+        # SECTION code to get logs from multiple vessels (assuming the argument imo is an iterable)
+        statement = text(
+            'SELECT imo, lat, lon, ts, id FROM vessel_logs WHERE imo IN :imos AND ts >= :start_ts AND ts <= :end_ts ORDER BY ts;'
+        )
+        result = conn.execute(
+            statement, {'imos': tuple(imo), 'start_ts': start_time, 'end_ts': end_time}
+        )
+        return hydrate_vessel_logs(result.fetchall())
 
 
 def hydrate_vessel_logs(raw_logs: Sequence[Row]) -> list[VesselLog]:
